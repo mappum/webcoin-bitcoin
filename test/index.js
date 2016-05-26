38094e7139f8da62c59543f6cf30a5eac7bc6b59
Matt Bell <mappum@gmail.com>
2016-05-26 15:45:23 -0700

    Added nyc test coverage

--- test/index.js
+++ test/index.js
@@ -2,4 +2,7 @@ var paramTests = require('webcoin-param-tests')
 var test = require('tape')
 var params = require('../')
 
-paramTests(params, test)
+test('param-tests', function (t) {
+  paramTests(params, t.test.bind(t))
+  t.end()
+})
