4f8da2ec44ffad6491d679f6644fc3b8568ce34e
Matt Bell <mappum@gmail.com>
2016-07-06 23:05:28 -0700

    Added versionbits params

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var params = require('webcoin-params')
 module.exports = params({
   blockchain: require('./lib/blockchain.js'),
   net: require('./lib/net.js'),
-  wallet: require('./lib/wallet.js')
+  wallet: require('./lib/wallet.js'),
+  versionbits: require('./lib/versionbits.js')
 })
---
+++ src/versionbits.js
@@ -0,0 +1,12 @@
+module.exports = {
+  confirmationWindow: 2016,
+  activationThreshold: 1916,
+  deployments: [
+    {
+      bit: 0,
+      name: 'csv',
+      start: 1462060800,
+      timeout: 1493596800
+    }
+  ]
+}
