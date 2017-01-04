27a9c0d8cbcb36d5aa17636c70c20fb3ab299f72
Matt Bell <mappum@gmail.com>
2017-01-03 17:28:11 -0800

    Added segwit versionbits constants

--- src/versionbits.js
+++ src/versionbits.js
@@ -1,12 +1,20 @@
 module.exports = {
   confirmationWindow: 2016,
   activationThreshold: 1916,
+  startHeight: 409248,
   deployments: [
     {
       bit: 0,
-      name: 'csv',
-      start: 1462060800,
-      timeout: 1493596800
+      id: 'csv',
+      name: 'CHECKSEQUENCEVERIFY',
+      start: 1462060800, // May 1st, 2016
+      timeout: 1493596800 // May 1st, 2017
+    }, {
+      bit: 1,
+      id: 'segwit',
+      name: 'Segregated Witness',
+      start: 1479168000, // November 15th, 2016
+      timeout: 1510704000 // November 15th, 2017
     }
   ]
 }
